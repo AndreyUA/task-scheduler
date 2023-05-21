b8c607ad15321b2bb692572f5187358c28eb2595
AndreyUA <infernalsteel666@gmail.com>
2023-05-21 20:19:32 +0300

    feat: tested signals with basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
+  imports: [ReactiveFormsModule],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  count = signal<number>(0);
+  form!: FormGroup;
+
+  ngOnInit() {
+    this.form = new FormGroup<any>({
+      counter: new FormControl<number>(0, [Validators.min(0)]),
+    });
+  }
+
+  increment(): void {
+    this.count.update((currentValue) => currentValue + 1);
+  }
+
+  decrement(): void {
+    this.count.update((currentValue) => currentValue - 1);
+  }
+
+  onSubmit(): void {
+    this.count.set(+this.form.get('counter')?.value);
+  }
+}
+
+// TODO:
+// https://angular.io/guide/signals
