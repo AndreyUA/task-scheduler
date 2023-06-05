e85484090a85ab5d6ca04be01f9918930fcde13d
AndreyUA <infernalsteel666@gmail.com>
2023-06-05 19:58:49 +0300

    feat: very basic dashboard implementation

--- src/app/feature/dashboard/dashboard.component.ts
+++ src/app/feature/dashboard/dashboard.component.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Task } from '../../core/models/task';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextareaModule,
+    ButtonModule,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  tasks = signal<Task[]>([
+    {
+      body: 'test 1',
+      creationDate: new Date(),
+    },
+    {
+      body: 'test 2',
+      creationDate: new Date(),
+    },
+  ]);
+  form = new FormGroup({
+    taskBody: new FormControl<string>('', [Validators.required]),
+  });
+
+  onSubmit(): void {
+    if (!this.form.get('taskBody')?.value) {
+      return;
+    }
+
+    this.tasks.update((currentTasks) => [
+      {
+        body: this.form.get('taskBody')!.value!.toString(),
+        creationDate: new Date(),
+      },
+      ...currentTasks,
+    ]);
+
+    this.form.reset();
+  }
+}
