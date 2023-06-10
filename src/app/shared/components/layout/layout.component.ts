db8717f761ed3dfe3d107afd27f2d8633b976897
AndreyUA <infernalsteel666@gmail.com>
2023-06-10 16:57:57 +0300

    feat: created header component

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FooterComponent],
+  imports: [CommonModule, RouterOutlet, FooterComponent, HeaderComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
