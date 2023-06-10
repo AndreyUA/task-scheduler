984265b84bb4664044fdec7cb219fa8270b42194
AndreyUA <infernalsteel666@gmail.com>
2023-06-10 16:48:42 +0300

    feat: footer component

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+  standalone: true,
+  imports: [ToolbarModule],
+})
+export class FooterComponent {
+  constructor() {}
+}
--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
